f39e174c46aa5f6d2b93c9cf871d30c74a890753
S-A-T-Y-A <satyanandan846@gmail.com>
2025-02-13 22:09:21 -0500

    Aded events screen and MapScreen with category selection for events and places

---
+++ frontend/app/(tabs)/(home)/filterTabs/others/Others.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { View, Text, ScrollView } from "react-native";
+
+const Others = () => {
+  return (
+    <ScrollView style={{ padding: 10 }}>
+      <Text style={{ fontSize: 18 }}>🎭 Miscellaneous Activities</Text>
+      <Text>- Shopping</Text>
+      <Text>- Local Workshops</Text>
+      <Text>- Nightlife</Text>
+    </ScrollView>
+  );
+};
+
+export default Others;
