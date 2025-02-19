4c2fe1f91ea221958686d391d97f7a572c987024
S-A-T-Y-A <satyanandan846@gmail.com>
2025-02-19 18:29:49 -0500

    Update .gitignore to exclude screenshots and .DS_store; remove commented code from index.js and clean up README.md

--- frontend/app/index.js
+++ frontend/app/index.js
@@ -2,27 +2,14 @@ import React, { useState } from "react";
 
 // Importing the login and signup components from their respective folders
 import LoginScreen from "./(auth)/login";
-// import SignUpPage from "./(auth)/signup";
 
 export default function Index() {
   const [isSignIn, setIsSignIn] = useState(false); // State to manage view
 
   // Toggle between login and signup (signin) screens
-  const toggleScreen = () => setIsSignIn(!isSignIn);
+  //const toggleScreen = () => setIsSignIn(!isSignIn);
 
   return (
-    // <div>
-    //   {/* Conditionally render Login or SignUp screen */}
-    //   {isSignIn ? <SignUpScreen /> : <LoginScreen />}
-
-    //   {/* Add a button to toggle between Login and SignUp */}
-    //   <button onClick={toggleScreen}>
-    //     {isSignIn ? "Go to Login" : "Go to Sign Up"}
-    //   </button>
-    // </div>
-
-    // <SignUpPage />
-
     <LoginScreen />
   );
 }
